7788aa151931e5662eea83457fea8251ad7d6b16
chris9797 <christends97@gmail.com>
2024-12-06 15:05:22 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,85 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+st.set_page_config(page_title="Mutual Fund Dashboard",
+                   page_icon=":bar_chart:",
+                   layout="centered")
+
+df = pd.read_excel('quad_data.xlsx')
+
+st.sidebar.header("Filter:")
+
+df = df[df['P_Oct-24'] != 0].copy()
+
+df = df[df['P_Nov-23'] != 0].copy()
+
+df.reset_index(drop=True, inplace=True)
+
+df['price_change'] = round(((df['P_Oct-24'] - df['P_Nov-23'])/df['P_Nov-23']) * 100)
+
+df['qty_change'] = round(((df['S_Oct-24'] - df['S_Nov-23'])/df['S_Nov-23']) * 100)
+
+stocks = st.sidebar.multiselect(
+    "Select the stocks:",
+    options=df['Stocks'].unique(),
+    default=df['Stocks'].unique()[0:5]
+)
+
+sectors = st.sidebar.multiselect(
+    "Select the sectors:",
+    options=df['Sector'].unique(),
+    default=df['Sector'].unique()[0:5]
+)
+
+indices = st.sidebar.multiselect(
+    "Select the indices:",
+    options=df['Indices'].unique(),
+    default=df['Indices'].unique()[0:5]
+)
+
+st.title(":bar_chart: Mutual Fund Dashboard")
+st.markdown('##')
+
+st.text("Selection:")
+
+df_selection = df.query(
+    "Stocks == @stocks or (Sector == @sectors and Indices == @indices)"
+)
+
+st.dataframe(df_selection)
+
+scatter_pl = px.scatter(
+    df_selection,
+    x='qty_change',
+    y='price_change',
+    title='Price vs Quantity Change %',
+    text='Stocks'
+)
+
+# Define the midpoints for the quadrants
+x_mid = 0
+y_mid = 0
+
+# Add quadrant lines
+
+mx = max(abs(df_selection.qty_change.min()), abs(df_selection.qty_change.max()))
+my = max(abs(df_selection.price_change.min()), abs(df_selection.price_change.max()))
+
+scatter_pl.update_layout(
+    xaxis=dict(title='Quantity Change %', range=[-mx, mx], zeroline=True, zerolinecolor='white'),
+    yaxis=dict(title='Price Change %', range=[-my, my], zeroline=True, zerolinecolor='white'),
+    shapes=[
+        # Vertical line
+        dict(type='line', x0=0, y0=-my, x1=0, y1=my, line=dict(color='white', width=1, dash='dot')),
+        # Horizontal line
+        dict(type='line', x0=-mx, y0=0, x1=mx, y1=0, line=dict(color='white', width=1, dash='dot'))
+    ]
+)
+
+scatter_pl.update_traces(
+    textposition='middle right',  # Position text to the right of the points
+    textfont=dict(size=10, color='white')
+)
+
+st.plotly_chart(scatter_pl)
